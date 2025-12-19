df8419e89f5c4efcbb7f66cb7d3612e4e9686824
SURIYA <suriyamail273@gmail.com>
2025-12-19 20:38:55 +0530

    Delete frontend/postcss.config.js

--- frontend/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// frontend/postcss.config.js - NEW/CORRECT
-
-module.exports = {
-  plugins: {
-    // 💡 Use the new dedicated PostCSS package name
-    '@tailwindcss/postcss': {}, 
-    'autoprefixer': {},
-  },
-}
