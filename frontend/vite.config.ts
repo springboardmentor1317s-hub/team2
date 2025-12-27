63f2473475035532e0fbdc437c2576c02da7022a
SAI-VENKAT-PALLAPU <saivenkatpallapu@gmail.com>
2025-12-27 13:41:27 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+  import tailwindcss from '@tailwindcss/vite';
 
   export default defineConfig({
     plugins: [react(),
