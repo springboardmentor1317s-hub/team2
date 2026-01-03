8b6153189d29cd1bac18290f8a09169535ffba35
Ayan Ansari <mayan9917@gmail.com>
2026-01-04 00:53:37 +0530

    enhanced db connection with timeout setting

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, {
+      // If it can't connect in 5 seconds, throw an error immediately
+      serverSelectionTimeoutMS: 5000,
+      // Close the socket if it's inactive for 45s
+      socketTimeoutMS: 45000,
+    })
     .then(() => console.log("MongoDB connected successfully!"))
-    .catch((err) => console.error("MongoDB connection error:", err));
+    .catch((err) => console.error("MongoDB connection err:", err));
 };
