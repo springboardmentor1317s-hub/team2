1f6aa400f83e2e5b4b60fbf21fa0cd237d457fd5
Ayan Ansari <mayan9917@gmail.com>
2026-01-09 11:48:09 +0530

    added toast component for showing message

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeProvider } from './context/ThemeContext'; // <-- IMPORT THIS
-const rootElement = document.getElementById('root');
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "./context/ThemeContext"; // <-- IMPORT THIS
+const rootElement = document.getElementById("root");
+
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ThemeProvider>
-    <App />
+      <App />
+      <Toaster position="top-right" richColors />
     </ThemeProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
