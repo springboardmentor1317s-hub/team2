8e19abfbb16a310068da802ff5a543904e60e2e4
Ayan Ansari <mayan9917@gmail.com>
2026-01-12 14:01:19 +0530

    defined handler functions for getting and completing user profile

---
+++ backend/controllers/userController.js
@@ -0,0 +1,61 @@
+const User = require("../models/User");
+
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      res
+        .status(401)
+        .json({ success: false, message: "User does not exists!" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Login successful!",
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        university: user.university,
+        role: user.role,
+        profilePicture: user.profilePicture,
+      },
+    });
+  } catch (error) {
+    console.error("Error while getting user:", error.message);
+    res
+      .status(500)
+      .json({ success: false, message: "Error while getting user" });
+  }
+};
+const updateProfile = async (req, res) => {
+  const { role, university } = req.body;
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      res
+        .status(401)
+        .json({ success: false, message: "User does not exists!" });
+    }
+    user.role = role;
+    user.university = university;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful!",
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        university: user.university,
+        role: user.role,
+        profilePicture: user.profilePicture,
+      },
+    });
+  } catch (error) {
+    console.error("Error updating profile:", error.message);
+    res.status(500).json({ success: false, message: "Error updating profile" });
+  }
+};
+
+module.exports = { getUser, updateProfile };
