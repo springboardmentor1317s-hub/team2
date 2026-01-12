6b5aa476cc577cc1b3d8508129b9d2ea4b36d909
Ayan Ansari <mayan9917@gmail.com>
2026-01-12 13:55:50 +0530

    defined routes for google and github login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,12 @@
 // backend/routes/auth.js
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authController");
+const {
+  loginUser,
+  loginWithGoogle,
+  registerUser,
+  redirectToAuthURL,
+  loginWithGithub,
+} = require("../controllers/authController");
 
 const router = express.Router();
 
@@ -9,6 +15,9 @@ const router = express.Router();
 // @access  Public
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/google", loginWithGoogle);
+router.get("/github", redirectToAuthURL);
+router.get("/github/callback", loginWithGithub);
 
 // @route   POST /api/auth/login
 // @desc    Authenticate user & get token
