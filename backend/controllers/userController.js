9fe831e690878b89a6578a336471866f931140a5
Ayan Ansari <mayan9917@gmail.com>
2026-01-15 11:11:28 +0530

    fixed the modal opening issue

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,18 +8,33 @@ const getUser = async (req, res) => {
         .status(401)
         .json({ success: false, message: "User does not exists!" });
     }
-    res.status(200).json({
-      success: true,
-      message: "Login successful!",
-      user: {
-        id: user._id,
-        fullName: user.fullName,
-        email: user.email,
-        university: user.university,
-        role: user.role,
-        profilePicture: user.profilePicture,
-      },
-    });
+    // if user has a university it means they have completed their profile
+    if (user.university) {
+      res.status(200).json({
+        success: true,
+        message: "Login successful!",
+        user: {
+          id: user._id,
+          fullName: user.fullName,
+          email: user.email,
+          university: user.university,
+          role: user.role,
+          profilePicture: user.profilePicture,
+        },
+      });
+    } else {
+      // if user has no university it means they have not completed their profile
+      res.status(200).json({
+        success: true,
+        message: "Login successful!",
+        user: {
+          id: user._id,
+          fullName: user.fullName,
+          email: user.email,
+          profilePicture: user.profilePicture,
+        },
+      });
+    }
   } catch (error) {
     console.error("Error while getting user:", error.message);
     res
