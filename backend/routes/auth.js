09b696974f4088c1c1d0c246e2ec9902ff03ec25
Ayan Ansari <mayan9917@gmail.com>
2026-01-15 11:01:16 +0530

    added comments

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,6 +6,7 @@ const {
   registerUser,
   redirectToAuthURL,
   loginWithGithub,
+  logoutUser,
 } = require("../controllers/authController");
 
 const router = express.Router();
@@ -14,13 +15,26 @@ const router = express.Router();
 // @desc    Register a new user
 // @access  Public
 router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.post("/google", loginWithGoogle);
-router.get("/github", redirectToAuthURL);
-router.get("/github/callback", loginWithGithub);
-
 // @route   POST /api/auth/login
 // @desc    Authenticate user & get token
 // @access  Public
+router.post("/login", loginUser);
+// @route   POST /api/auth/google
+// @desc    Google authentication
+// @access  Public
+router.post("/google", loginWithGoogle);
+// @route   GET /api/auth/github
+// @desc    Github authentication
+// @access  Public
+router.get("/github", redirectToAuthURL);
+// @route   GET /api/auth/github/callback
+// @desc    Github authentication callback
+// @access  Public
+router.get("/github/callback", loginWithGithub);
+// @route   GET /api/auth/logout
+// @desc    Logout user
+// @access  Public
+router.get("/logout", logoutUser);
+
 
 module.exports = router;
--- backend/routes/registration.js
+++ backend/routes/registration.js
@@ -1,22 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authToken,verifyAdmin } = require('../middleware/auth');
-const { registerForEvent, getAllRegisteredEvents, updateRegistrationStatus, getMyRegisteredEvents } = require('../controllers/registrationController');
+const { authToken, verifyAdmin } = require("../middleware/auth");
+const {
+  registerForEvent,
+  getAllRegisteredEvents,
+  updateRegistrationStatus,
+  getMyRegisteredEvents,
+} = require("../controllers/registrationController");
 
 // 1. STUDENT: Register for an event
 // POST /api/registrations
-router.post('/', authToken, registerForEvent);
+router.post("/", authToken, registerForEvent);
 
 // 2. ADMIN: Get all registrations (to show on Admin Dashboard)
 // GET /api/registrations/all
-router.get('/all', authToken, verifyAdmin, getAllRegisteredEvents);
+router.get("/all", authToken, verifyAdmin, getAllRegisteredEvents);
 
 // 3. ADMIN: Approve or Reject a registration
 // PUT /api/registrations/:id/status
-router.put('/:id/status', authToken, verifyAdmin, updateRegistrationStatus);
+router.put("/:id/status", authToken, verifyAdmin, updateRegistrationStatus);
 
 // 4. STUDENT: Get my registrations (to see status and admin action)
 // GET /api/registrations/my
-router.get('/my', authToken, getMyRegisteredEvents);
+router.get("/my", authToken, getMyRegisteredEvents);
 
 module.exports = router;
